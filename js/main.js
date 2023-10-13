3756213f0373e42115750bbb9dca2e85ae0bc026
Yuen Zheng Chng <ychng@u90z01s02.42kl.edu.my>
2023-10-14 07:12:10 +0800

    initial commit for password page

--- js/main.js
+++ js/main.js
@@ -1,168 +1,100 @@
-var message;
-var elYearInput;
-
-message = "Where Do You See Yourself In 5 Years?";
-elYearInput = document.getElementById("year-input");
-elYearInput.value = message;
-
-function randomTick() 
-{
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function(checkbox) {
-        var randomValue = Math.random() >= 0.5;
-        checkbox.checked = randomValue;
-    });
+function hasNumbers(input) {
+    var regex = /\d/;
+    return regex.test(input);
 }
 
-document.addEventListener('DOMContentLoaded', randomTick);
-
-var flag;
-
-flag = 1;
-
-var checkboxes;
-
-checkboxes = document.querySelectorAll("input[type='checkbox']");
-
-function areAllCheckboxedUnchecked()
-{
-    var i;
-
-    i = 0;
-    while (i < checkboxes.length)
-    {
-        if (checkboxes[i].checked)
-        {
-            flag = 1;
-            return ;
-        }
-        i++;
-    }
-    flag = 0;
-    return ;
+function hasSymbols(input) {
+    var regex = /[!@#$%^&*(),.?":{}|<>]/;
+    return regex.test(input);
 }
 
-var elEntryForm;
-var images;
-
-elEntryForm = document.getElementById("entry-form");
-images =  document.querySelectorAll(".entry > img");
-
-function hasCharacter(value) {
-    return /[a-zA-Z]/.test(value);
-}
-
-function containsDigitsOneToNine(value) {
-    var pattern = /[1-79]/;
-    return pattern.test(value);
-}
-
-function handleEnterKey(event)
-{
-    if (event.key === "Enter")
-    {
-        event.preventDefault();
-        areAllCheckboxedUnchecked();
-        if (elYearInput.value == message)
-            flag = 1;
-        else if (hasCharacter(elYearInput.value))
-        {
-            flag = 1;
-            elYearInput.value = "numeric numbrs only, please"
-        }
-        else if (containsDigitsOneToNine(elYearInput.value))
-        {
-            flag = 1;
-            elYearInput.value = "ONLY 1-9"
+function countSymbols(input) {
+    var symbolCount = 0;
+    for (var i = 0; i < input.length; i++) {
+        var char = input.charAt(i);
+        if(/[!@#$%^&*(),.?":{}|<>]/.test(char)) {
+            symbolCount++;
         }
     }
-    if (flag == 0)
-    {
-        winner = document.querySelector("img[alt='dog(46)'");
-        winner.alt = "dgo(46)";
-    }
+    return symbolCount;
 }
 
-elEntryForm.addEventListener("keypress", handleEnterKey);
-
-
-var ctrlA = false;
-var cut = false;
-var ctrl = false;
-var selectionandbackspace = false;
-
-function lucky()
-{
-    if (ctrlA && cut && ctrl && selectionandbackspace)
-    {
-        console.log("YOU GOT LUCKY FIND IT!");
-        winner = document.querySelector("img[alt='dog(46)'");
-        winner.alt = "dgo(46)";
-    }
-}
-
-document.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.key === 'a' && document.activeElement.type === 'text') 
-    {
-        event.preventDefault();
-        randomTick();
-        ctrlA = true;
-        lucky();
-        alert("FIND ANOTHER WAY!");
-    }
-    if (event.metaKey && event.key === 'a' && document.activeElement.type === 'text') 
-    {
-        event.preventDefault(); // Prevent the default behavior of Command+A
-        randomTick();
-        ctrlA = true;
-        lucky();
-        alert("FIND ANNOTHER WAY!");
-    }
-    if ((event.metaKey || event.ctrlKey) && document.activeElement.type === 'text') 
-    {
-        switch(event.key) {
-            case 'ArrowLeft':
-                alert("So LAZYYYyyyy")
-                randomTick();
-                ctrl = true;
-                lucky();
-                break;
-            case 'ArrowRight':
-                alert("So LAZYYYY.")
-                randomTick();
-                ctrl = true;
-                lucky();
-                break;
-
+function hasAdjacentDuplicates(input) {
+    for (var i = 0; i < input.length - 1; i++) {
+        if (input[i] === input[i + 1]) {
+            return true;
         }
     }
+    return false;
+}
 
-});
+function hasTextBasedEmojis(input) {
+    var regex = /(:\)|:-\)|:\(|:-\(|;\)|;-\)|:-D|:D|:-d|:d|<3|>:o|>:O|:-P|:P|:-p|:p)/;
+    return regex.test(input);
+}
 
+function hasMalaysia(str) {
+    return str.includes("Malaysia");
+}
 
-document.addEventListener('keydown', function(event) {
-    var selection = window.getSelection();
-    if (selection && selection.toString().length > 0 && (event.key === 'Backspace' || event.key === 'Delete') && document.activeElement.type === 'text') {
-        event.preventDefault();
-        selectionandbackspace = true;
-        randomTick();
-        lucky();
-        alert("YOU ARR ALMOST THERE!");
+function hasKL(str) {
+    return str.includes("Kuala_Lumpur");
+}
+
+function has42kl(str) {
+    return str.includes("42kl");
+}
+
+function sumDigitsInString(str) {
+    var digitSum = 0;
+    for (var i = 0; i < str.length; i++) {
+        var char = str.charAt(i);
+        var digit = parseInt(char);
+
+        if (!isNaN(digit)) {
+            digitSum += digit;
+        }
     }
-});
+    return digitSum;
+}
 
-document.addEventListener('cut', function(event) {
-    event.preventDefault();
-    cut = true;
-    randomTick();
-    lucky();
-    alert("sorry");
-});
+function checkPassReq(input)
+{
+    switch (true)
+    {
+        case input.length < 10:
+            return "password must be " + (input.length + 1) + " characters long";
+        case !hasNumbers(input):
+            return "password must have a number";
+        case !hasSymbols(input):
+            return "password must have a symbol";
+        case countSymbols(input) < 5:
+            var symbol_count = countSymbols(input);
+            return "password must have " + (symbol_count + 1) + " symbols";
+        case hasAdjacentDuplicates(input):
+            return "remove the adjacent identical characters"
+        case !hasTextBasedEmojis(input):
+            return "password must have a text based emoji";
+        case !hasMalaysia(input):
+                return "password must include Malaysia"
+        case !hasKL(input):
+            return "password must include Kuala_Lumpur"
+        case !has42kl(input):
+            return "password must include 42kl"
+        case sumDigitsInString(input) < 42:
+            return "all digits must total up to 42 or more"
+        default:
+            return "weak password"
+    }
+}
 
-document.addEventListener('paste', function(e) {
-    var clipboardData = e.clipboardData || window.clipboardData;
-    var pastedText = clipboardData.getData('text');
-    var selection = window.getSelection();
-    if (pastedText.length < selection.toString().length)
-        alert("YOU FOUND A WAY! :]")
-});
+var password = document.querySelector("#password");
+password.addEventListener("input", function(e) {
+    var dynamic_paragraph = document.querySelector("#content > p");
+    if (dynamic_paragraph)
+        dynamic_paragraph.remove();
+    var input = e.target.value;
+    var paragraph = document.createElement('p');
+    paragraph.textContent = checkPassReq(input);
+    content.appendChild(paragraph);
+})
