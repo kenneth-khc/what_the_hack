a395a72c4e70a4dc917a8562251b6eb159fa78b8
TCheeMing <teohcheeming@gmail.com>
2023-10-15 11:47:30 +0800

    alert tab v1.1
    features:
    -draggable
    -duplication
    -close
    -sound
    -stack, z-index

--- js/alert.js
+++ js/alert.js
@@ -1,10 +1,14 @@
-function fixErrorTab() {
-    // Drag the pop-up tab, when click on the header
-    var header = document.querySelector(".pop-up > .header");
+// Drag the pop-up tab, when click on the header
+var pop_ups = document.querySelectorAll(".pop-up");
+var zIndexCounter = 1;
 
-    function onMouseDown(e)
-    {
-        var close_button = document.querySelector(".pop-up > .header > .controls > button[aria-label='Close']");
+function onMouseDown(e)
+{
+    var target = e.target
+    var pop_up = target.parentNode;
+
+    if (target.classList.contains("header")) {
+        var close_button = target.querySelector(".controls > button[aria-label='Close']");
         var close_button_rect = close_button.getBoundingClientRect();
         if (e.clientX >= close_button_rect.left
         && e.clientX <= close_button_rect.right
@@ -13,9 +17,9 @@ function fixErrorTab() {
             return; // If mouse is over close button, do nothing
         }
 
-        var pop_up = document.querySelector(".pop-up");
-        pop_up.style.position = "absolute";
-        header.style.cursor = "move";
+        target.style.cursor = "move";
+        // pop_up.style.zIndex = zIndexCounter;
+        // zIndexCounter++;
 
         var offsetX = e.clientX - pop_up.offsetLeft;
         var offsetY = e.clientY - pop_up.offsetTop;
@@ -23,63 +27,71 @@ function fixErrorTab() {
         function onMouseMove(e) {
             var x = e.pageX;
             var y = e.pageY;
-
+    
             pop_up.style.left = (x - offsetX) + "px";
             pop_up.style.top = (y - offsetY) + "px";
         }
-
+    
         function onMouseUp() {
             document.removeEventListener('mousemove', onMouseMove);
             document.removeEventListener('mouseup', onMouseUp);
-            header.style.cursor = "auto";
+            if (target.classList.contains("header")) {
+                target.style.cursor = "auto";
+            }
         }
 
         document.addEventListener('mousemove', onMouseMove);
         document.addEventListener('mouseup', onMouseUp);
-    }
-
-    header.addEventListener("mousedown", onMouseDown);
-
-    // Change cursor to draggable icon when click on the header
-    var pop_up = document.querySelector(".pop-up");
-
-    pop_up.addEventListener("mousemove", function(e) {
-        var rect = this.getBoundingClientRect();
-        var borderWidth = 6; // Adjust this value to match your border width
-
-        var isNearLeftEdge = e.clientX >= rect.left && e.clientX <= rect.left + borderWidth;
-        var isNearRightEdge = e.clientX >= rect.right - borderWidth && e.clientX <= rect.right;
-        var isNearTopEdge = e.clientY >= rect.top && e.clientY <= rect.top + borderWidth;
-        var isNearBottomEdge = e.clientY >= rect.bottom - borderWidth && e.clientY <= rect.bottom;
-
-        if (isNearLeftEdge || isNearRightEdge) {
-            this.style.cursor = "ew-resize";
-        } else if (isNearTopEdge || isNearBottomEdge) {
-            this.style.cursor = "ns-resize";
-        } else {
-            this.style.cursor = "auto";
-        }
-    });
-
-    // Delete pop-up tab when click on the fix button
-    var close_button = document.querySelector(".pop-up > .content > .buttons > button:first-child");
-
-    close_button.addEventListener("click", function(e) {
-        pop_up.remove();
-    })
-
-    // Minimize when click on the x button, set display to none
-    var minimize_button = document.querySelector(".pop-up > .header > .controls > button[aria-label='Close']");
-    var pop_up_body = document.querySelector(".pop-up > .content")
-    var original_display = pop_up_body.style.display;
-
-    minimize_button.addEventListener("click", function(e) {
-        if (pop_up_body.style.display == "none") {
-            pop_up_body.style.display = original_display;
-        } else {
-            pop_up_body.style.display = "none";
-        }
-    })
+    } // End of header settings
 }
 
-fixErrorTab();
+function features(pop_up) {
+    pop_up.addEventListener("mousedown", function(e) {
+        onMouseDown(e);
+    });
+    // Click functionality
+    pop_up.addEventListener("click", function(e) {
+        var target = e.target;
+        // Duplicate functionaility
+        if (target.getAttribute("aria-label") == "Close")
+        {
+            var pop_up = target.closest(".pop-up");
+            var original_div = pop_up.cloneNode(true);
+
+            // Generate random top and left values
+            var randomTop = Math.floor(Math.random() * (window.innerHeight - original_div.offsetHeight));
+            var randomLeft = Math.floor(Math.random() * (window.innerWidth - original_div.offsetWidth));
+
+            original_div.style.position = "absolute";
+            original_div.style.top = randomTop + "px";
+            original_div.style.left = randomLeft + "px";
+
+            document.body.insertBefore(original_div, pop_up);
+            features(original_div);
+
+            var audio = pop_up.querySelector(".header > .controls > #mysound");
+            function toggleAudio() {
+                audio.currentTime = 0;
+                audio.play();
+            }
+            toggleAudio();
+        }
+    });
+    // Close functionality
+    var closeButton = pop_up.querySelector(".content > .buttons > button:first-child");
+    closeButton.addEventListener("click", function() {
+        var pop_up = this.closest(".pop-up");
+        pop_up.parentNode.removeChild(pop_up);
+    });
+
+    // put above the other tab, by adding z-index
+    pop_up.addEventListener("mousedown", function() {
+        pop_up.style.zIndex = zIndexCounter;
+        zIndexCounter++;
+    });
+}
+
+pop_ups.forEach(function(pop_up) {
+    features(pop_up);
+})
+
