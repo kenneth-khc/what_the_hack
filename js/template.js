5f898ca84615d7a25cfb3ecad1a907dca3caa8c7
Chee ming Teoh <cteoh@u81z05s04.42kl.edu.my>
2023-10-17 03:42:44 +0800

    add password requirement and scroll in password requirement

--- js/template.js
+++ js/template.js
@@ -85,6 +85,8 @@ function features(pop_up) {
         pop_up.style.zIndex = zIndexCounter;
         zIndexCounter++;
     });
+    // To do password functionality
+    login_func();
 }
 
 pop_ups.forEach(function(pop_up) {
@@ -160,17 +162,129 @@ function type() {
         let randomLeft = Math.floor(Math.random() * (bodyWidth - 100));
         login.style.top = randomTop + "px";
         login.style.left = randomLeft + "px";
-        login.style.display = "initial";
         randomTop = Math.floor(Math.random() * (bodyHeight - 100));
         randomLeft = Math.floor(Math.random() * (bodyWidth - 100));
         requirement.style.top = randomTop + "px";
         requirement.style.left = randomLeft + "px";
-        requirement.style.display = "initial";
+        setTimeout(function() {
+            login.style.display = "initial";
+            requirement.style.display = "initial";
+        }, 100);
     }
 };
-
 
 document.addEventListener("click", type);
 document.addEventListener("click", function() {
     song1.play();
-})
+});
+
+
+function login_func() {
+    function hasNumbers(input) {
+        var regex = /\d/;
+        return regex.test(input);
+    }
+    
+    function hasSymbols(input) {
+        var regex = /[!@#$%^&*(),.?":{}|<>]/;
+        return regex.test(input);
+    }
+    
+    function countSymbols(input) {
+        var symbolCount = 0;
+        for (var i = 0; i < input.length; i++) {
+            var char = input.charAt(i);
+            if(/[!@#$%^&*(),.?":{}|<>]/.test(char)) {
+                symbolCount++;
+            }
+        }
+        return symbolCount;
+    }
+    
+    function hasAdjacentDuplicates(input) {
+        for (var i = 0; i < input.length - 1; i++) {
+            if (input[i] === input[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    function hasTextBasedEmojis(input) {
+        var regex = /(:\)|:-\)|:\(|:-\(|;\)|;-\)|:-D|:D|:-d|:d|<3|>:o|>:O|:-P|:P|:-p|:p)/;
+        return regex.test(input);
+    }
+    
+    function hasMalaysia(str) {
+        return str.includes("Malaysia");
+    }
+    
+    function hasKL(str) {
+        return str.includes("Kuala_Lumpur");
+    }
+    
+    function has42kl(str) {
+        return str.includes("42kl");
+    }
+    
+    function sumDigitsInString(str) {
+        var digitSum = 0;
+        for (var i = 0; i < str.length; i++) {
+            var char = str.charAt(i);
+            var digit = parseInt(char);
+    
+            if (!isNaN(digit)) {
+                digitSum += digit;
+            }
+        }
+        return digitSum;
+    }
+    
+    function checkPassReq(input)
+    {
+        switch (true)
+        {
+            case input.length < 10:
+                return "password must be " + (input.length + 1) + " characters long";
+            case !hasNumbers(input):
+                return "password must have a number";
+            case !hasSymbols(input):
+                return "password must have a symbol";
+            case countSymbols(input) < 5:
+                var symbol_count = countSymbols(input);
+                return "password must have " + (symbol_count + 1) + " symbols";
+            case hasAdjacentDuplicates(input):
+                return "remove the adjacent identical characters"
+            case !hasTextBasedEmojis(input):
+                return "password must have a text based emoji";
+            case !hasMalaysia(input):
+                    return "password must include Malaysia"
+            case !hasKL(input):
+                return "password must include Kuala_Lumpur"
+            case !has42kl(input):
+                return "password must include 42kl"
+            case sumDigitsInString(input) < 42:
+                return "all digits must total up to 42 or more"
+            default:
+                return "weak password"
+        }
+    }
+    
+    var password = document.querySelector("#password");
+    var requirement = document.querySelector("#requirement");
+    password.addEventListener("input", function(e) {
+        // var dynamic_paragraph = document.querySelector("#requirement > .content > ul > li");
+        // if (dynamic_paragraph)
+        //     dynamic_paragraph.remove();
+        var input = e.target.value;
+        var paragraph = document.createElement('li');
+        paragraph.textContent = checkPassReq(input);
+        document.querySelector("#requirement > .content > ul").appendChild(paragraph);
+
+        // Scroll for
+        var scrollableDiv = document.querySelector("#requirement .content");
+        scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
+    })
+};
+
+login_func();
